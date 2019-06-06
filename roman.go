ae9071f4c0b979172365366a96196ba148ba38c4
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2019-06-07 00:37:16 +0200

    Add String() impl that use the additive notation

--- roman.go
+++ roman.go
@@ -17,8 +17,39 @@ const (
 	M       = 1000
 )
 
+// String displays the roman numeral in additive notation:
+// XVIIII instead of XIX.
 func (r Roman) String() string {
-	return ""
+	var val = int64(r)
+	var output string
+	for val > 0 {
+		fmt.Println("output:", output, "val:", val)
+		switch {
+		case val-int64(M) >= 0:
+			val -= int64(M)
+			output += "M"
+		case val-int64(D) >= 0:
+			val -= int64(D)
+			output += "D"
+		case val-int64(C) >= 0:
+			val -= int64(C)
+			output += "C"
+		case val-int64(L) >= 0:
+			val -= int64(L)
+			output += "L"
+		case val-int64(X) >= 0:
+			val -= int64(X)
+			output += "X"
+		case val-int64(V) >= 0:
+			val -= int64(V)
+			output += "V"
+		case val-int64(I) >= 0:
+			val -= int64(I)
+			output += "I"
+		}
+	}
+
+	return output
 }
 
 var roman2arabic = map[string]Roman{
